4934e9ef47314692371f67427901d84dbd6b3a9c
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-24 17:18:51 -0600

    basic format of profile but with not very impressive css stylings

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,22 +3,73 @@ import React from 'react';
 function Profile() {
   return (
     <div>
-      <div class="columns is-mobile">
-        <div class="column is-half is-offset-one-quarter"><img src="http://placekitten.com/g/200/300"></img></div>
-      </div>
+      <div class="columns avatar">
+        <div class="columns is-mobile">
+          <div class="column"><img src="http://placekitten.com/g/200/300"></img></div>
+        </div>
+        <div class="media-content">
+          <p class="title">Kitty Cat</p>
+          <p class="subtitle">
+            Meow meow meow meow, meow meow meow. Meow. Meow meow meow meow.
+          </p>
+          <p class="subtitle">
+            Gender: <span id="gender">Female</span> <br></br>
+            Birthday: <span id="date">06/24/2021</span> <br></br>
+            --------------------
+          </p>
+          <div class="columns is-mobile">
+            <div class="column">
+              <h2 class="title">
+                Favorites
+              </h2>
+              <ol>
+                <li>Taco Bell</li>
+                <li>Taco Bell</li>
+                <li>Taco Bell</li>
+                <li>Taco Bell</li>
+                <li>Taco Bell</li>
+              </ol>
+            </div>
 
-      <div class="columns is-mobile">
-        <div class="column is-three-fifths is-offset-one-fifth">Favorites</div>
-      </div>
+          </div>
+          <div class="columns is-mobile">
+            <div class="column">
 
-      <div class="columns is-mobile">
-        <div class="column is-4 is-offset-8">HEY</div>
-      </div>
+              <h2 class="title">
+                Posts/Comments
+              </h2>
+              
+              <article>
+                <div class="post">
+                  <a href="/"></a>
+                  <span class="post-title">Test Title</span>
+                </div>
+                <div class="post-text">
 
-      <div class="columns is-mobile">
-        <div class="column is-11 is-offset-1">HEY</div>
+                  This is the body of the post.
+
+                </div>
+                <div class="post-user">
+                  By User on
+                  date
+                  
+                </div>
+              </article>
+            </div>
+          </div>
+
+          <div class="columns is-mobile">
+            <div class="column">
+              <h2 class="title">
+                Extra? Friends List
+              </h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
+
 
 
   );
