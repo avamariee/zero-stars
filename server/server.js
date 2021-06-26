0b7d4b4a07ca793e1cb4676f385e3fc506fb0d51
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-26 16:03:25 -0600

    trying to set up hooks to use api

--- server/server.js
+++ server/server.js
@@ -16,58 +16,6 @@ const server = new ApolloServer({
 });
 
 
-// API STUFF HERE?
-
-// 'use strict';
-
-// const yelp = require('yelp-fusion')
-// require('dotenv').config();
-// const YELP_KEY = process.env.YELP_KEY;
-
-
-// const searchRequest = {
-//     term: 'Taco Bell',
-//     location: 'salt lake city, ut'
-// };
-
-// const client = yelp.client(YELP_KEY);
-
-// client.search(searchRequest).then(response => {
-//     const firstResult = response.jsonBody.businesses[0];
-//     const prettyJson = JSON.stringify(firstResult, null, 4);
-//     console.log(prettyJson);
-
-// }).catch(e => {
-//     console.log(e);
-// })
-
-// let getYelp = function () {
-//   let apiUrl = "https://api.yelp.com/v3/search/taco-bell"
-
-//   // make a request to the URL
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       // if the request was successful
-//       if (response.ok) {
-//         console.log(response);
-//         response.json().then(function (data) {
-//           console.log(data);
-
-//         })
-//       } else {
-//         console.log("There was an error: " + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       console.log("Unable to connect to Yelp.")
-//     })
-// };
-
-// getYelp();
-
-// //////
-
-
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
