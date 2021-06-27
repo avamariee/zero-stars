378152598fea6e4835dde3b52df189dee9b22bad
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-26 18:59:14 -0600

    imported what I had into the Search component, stopping for now to let front end catch up

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
+// import yelp api functionality
+import {useRestaurantSearch} from '../../hooks/yelp-api/useRestaurantSearch';
+
 function Search() {
+
+  // yelp API
+  const [restaurants, amountResults, searchParams, setSearchparams] = useRestaurantSearch()
+
   return (
     <div>
       <h1>Test</h1>
