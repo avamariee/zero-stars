08e7d2cbdec1aa7df7603c2d17fd2c4847a51f16
L-Barrios <luisiannabarrios@gmail.com>
2021-06-26 19:45:31 -0600

    styling on footer fixed

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className = 'content has-text-centered'>
+    <footer className = 'content has-text-center'>
         <div className = 'is-family-secondary is-size-6'>
             Data pulled from yelp. Created by amazing people.✨
         </div>
