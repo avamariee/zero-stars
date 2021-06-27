19689f83d617054dccfbb4508da82ddeae806540
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-27 14:51:41 -0600

    fixed errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter as Router/* , Switch, Route */ } from 'react-router-dom';
 
+/* import components */
 import Profile from './components/Profile';
 import AppNav from './components/Navbar';
+import Landing from './components/Landing'
+import Search from './components/Search'
+// import Footer from './components/Footer'
+
 import '././styles.css'
 
 const client = new ApolloClient({
@@ -21,34 +26,39 @@ const client = new ApolloClient({
 });
 
 // getters and setters for state for page
-const [currentPage, setCurrentPage] = useState("landing");
-
-// switch case to display each page
-const displayPage = () => {
-  switch (currentPage) {
-
-    case "landing":
-      return <Landing />;
-
-    case "search":
-      return <Search />;
-
-    case "profile":
-      return <Profile />;
-
-    default:
-      return <Landing />
-
-  }
-}
 
 function App() {
+
+const [currentPage, setCurrentPage] = useState("landing");
+  
+  // switch case to display each page
+  const displayPage = () => {
+    switch (currentPage) {
+  
+      case "landing":
+        return <Landing />;
+  
+      case "search":
+        return <Search />;
+  
+      case "profile":
+        return <Profile />;
+  
+      default:
+        return <Landing />
+  
+    }
+  }
+
   return (
 
     <ApolloProvider client={client}>
       <Router>
         <>
-          <AppNav />
+          <AppNav 
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          />
           <div>
             <main>
               {/* display page from switch case above */}
@@ -56,6 +66,8 @@ function App() {
             </main>
           </div>
         </>
+        {/* add footer once merged into develop */}
+        {/* <Footer></Footer> */}
       </Router>
     </ApolloProvider>
   );
