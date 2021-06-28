48b48fb21636950bcc4f4718b2642946e31e1e85
L-Barrios <luisiannabarrios@gmail.com>
2021-06-28 17:55:24 -0600

    not great landing page

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -3,7 +3,33 @@ import React from 'react';
 function Landing() {
   return (
     <div>
+
+    
+    <header className ="section has-text-centered">
+      <h1 className ="title is-1"> The Best of the Worst!</h1>
+      <h2 className = "subtitle is-2">★☆☆☆☆</h2>
+    </header>
+    
+    
+
+    
+    <div className = "landingSearch">
+    <div className="field has-addons ">
+      <div class="columns is-mobile is-centered">
+        <div className="control searchBar">
+          <input className="input" type="text" placeholder="Find a restaurant" />
+        </div>
+        <div className="control">
+          <a className="button is-info">
+          Search
+          </a>
+        </div>
+        </div>
     </div>
+    </div>
+    </div>
+
+
   );
 }
 
