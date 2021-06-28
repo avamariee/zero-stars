e8e24e4965eed11fc37c9257bd2c23040ab939fe
Tess Austin <tessaustin92@gmail.com>
2021-06-28 15:42:19 -0600

    trying different methods to get log in signup working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router/* , Switch, Route */ } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 /* import components */
 import Profile from './components/Profile';
@@ -11,7 +11,7 @@ import Search from './components/Search'
 // import Footer from './components/Footer'
 
 import '././styles.css'
-import Switch from 'react-bootstrap/esm/Switch';
+/* import Switch from 'react-bootstrap/esm/Switch'; */
 
 const client = new ApolloClient({
   request: operation => {
@@ -30,7 +30,7 @@ const client = new ApolloClient({
 
 function App() {
 
-const [currentPage, setCurrentPage] = useState("landing");
+/* const [currentPage, setCurrentPage] = useState("landing");
   
   // switch case to display each page
   const displayPage = () => {
@@ -49,29 +49,43 @@ const [currentPage, setCurrentPage] = useState("landing");
         return <Landing />
   
     }
-  }
+  } */
 
   return (
 
     <ApolloProvider client={client}>
       <Router>
         <>
-          <AppNav 
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-          />
-          <div>
-            <main>
-              {/* display page from switch case above */}
-              {displayPage()}
-            </main>
-          </div>
+          <AppNav />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/search' component={Search} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
         </>
-        {/* add footer once merged into develop */}
-        {/* <Footer></Footer> */}
       </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
+
+
+{/* <ApolloProvider client={client}>
+<Router>
+  <>
+    <AppNav 
+    currentPage={currentPage}
+    setCurrentPage={setCurrentPage}
+    />
+    <div>
+      <main> */}
+        {/* display page from switch case above */}
+     /*    {displayPage()}
+      </main>
+    </div>
+  </> */
+  {/* add footer once merged into develop */}
+  {/* <Footer></Footer> */}
+/* </Router>
+</ApolloProvider> */
