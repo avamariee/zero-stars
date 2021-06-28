f7d88f8ccc8af2bfaa697a0a9d6c56f61f769d4d
Tess Austin <tessaustin92@gmail.com>
2021-06-28 14:56:21 -0600

    undid user.js b/c npm watch did not like it

--- server/models/User.js
+++ server/models/User.js
@@ -29,31 +29,29 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    toJSON: {
-        virtuals: true,
-    },
-}
+
+    
     // user can favorite the restaurant with terrible reviews
     // favorites: [favoritesSchema]
-);
+});
 
 // add favorites later?
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function(next) {
     if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
     }
-
+  
     next();
-});
-
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function (password) {
+  });
+  
+  // compare the incoming password with the hashed password
+  userSchema.methods.isCorrectPassword = async function(password) {
     return bcrypt.compare(password, this.password);
-};
-
-const User = model('User', userSchema);
-
-module.exports = User;
+  };
+  
+  const User = mongoose.model('User', userSchema);
+  
+  module.exports = User;
