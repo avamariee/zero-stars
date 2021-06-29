fcf960cdf11089490b542e2e50662056677ee737
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-28 20:22:47 -0600

    each card displays with restaurant name

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -58,9 +58,10 @@ function Search() {
       </div>
 
       <div>
-        {badRestaurants.map(rest => <p>{rest.name}</p>)}
       </div>
       <div class="box">
+        {badRestaurants.map(rest => (
+
         <div class="card">
           <div class="card-image">
           </div>
@@ -72,7 +73,7 @@ function Search() {
                 </figure>
               </div>
               <div class="media-content">
-                <p class="title is-4 is-black">{badRestaurants.map(rest => <h1>{ (rest.name) }</h1>)}</p>
+                <p class="title is-4 is-black">{rest.name}</p>
                 <p class="subtitle is-6">@rest1</p>
               </div>
             </div>
@@ -88,172 +89,8 @@ function Search() {
           </div>
 
         </div>
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
-            </figure>
-          </div>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-              </p>
-            </div>
-            <nav class="level is-mobile">
-              <div class="level-left">
-                <a class="level-item" aria-label="reply">
-                  <span class="icon is-small">
-                    <i class="fas fa-reply" aria-hidden="true" />
-                  </span>
-                </a>
-                <a class="level-item" aria-label="retweet">
-                  <span class="icon is-small">
-                    <i class="fas fa-retweet" aria-hidden="true" />
-                  </span>
-                </a>
-                <a class="level-item" aria-label="like">
-                  <span class="icon is-small">
-                    <i class="fas fa-heart" aria-hidden="true" />
-                  </span>
-                </a>
-              </div>
-            </nav>
-          </div>
-        </article>
-      </div>
-      <div class="box">
-        <div class="card">
-          <div class="card-image">
-            {/* <figure class="image is-1by1">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure> */}
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4 is-black">Search Result from Yelp 2</p>
-                <p class="subtitle is-6">@rest2</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1" />11:09 PM - 1 Jan 2016
-            </div>
-          </div>
-        </div>
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
-            </figure>
-          </div>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-              </p>
-            </div>
-            <nav class="level is-mobile">
-              <div class="level-left">
-                <a class="level-item" aria-label="reply">
-                  <span class="icon is-small">
-                    <i class="fas fa-reply" aria-hidden="true" />
-                  </span>
-                </a>
-                <a class="level-item" aria-label="retweet">
-                  <span class="icon is-small">
-                    <i class="fas fa-retweet" aria-hidden="true" />
-                  </span>
-                </a>
-                <a class="level-item" aria-label="like">
-                  <span class="icon is-small">
-                    <i class="fas fa-heart" aria-hidden="true" />
-                  </span>
-                </a>
-              </div>
-            </nav>
-          </div>
-        </article>
-      </div>
-
-      <div class="box">
-        <div class="card">
-          <div class="card-image">
-            {/* <figure class="image is-1by1">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure> */}
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4 is-black">Search Result from Yelp 3</p>
-                <p class="subtitle is-6">@rest3</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1" />11:09 PM - 1 Jan 2016
-            </div>
-          </div>
-        </div>
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
-            </figure>
-          </div>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-              </p>
-            </div>
-            <nav class="level is-mobile">
-              <div class="level-left">
-                <a class="level-item" aria-label="reply">
-                  <span class="icon is-small">
-                    <i class="fas fa-reply" aria-hidden="true" />
-                  </span>
-                </a>
-                <a class="level-item" aria-label="retweet">
-                  <span class="icon is-small">
-                    <i class="fas fa-retweet" aria-hidden="true" />
-                  </span>
-                </a>
-                <a class="level-item" aria-label="like">
-                  <span class="icon is-small">
-                    <i class="fas fa-heart" aria-hidden="true" />
-                  </span>
-                </a>
-              </div>
-            </nav>
-          </div>
-        </article>
+        ))}
+        
       </div>
 
 
