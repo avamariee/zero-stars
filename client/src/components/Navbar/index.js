fb6d5ac622c3eaff4ac3070476eeea8038a303d2
Tess Austin <tessaustin92@gmail.com>
2021-06-28 18:21:13 -0600

    tried to get zero star logo to show next to title

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,40 +7,45 @@ import Login from '../Login';
 import Auth from '../../utils/auth';
 import '../../styles.css'
 
+
 const AppNav = () => {
     // set modal display state
+
+
     const [showModal, setShowModal] = useState(false);
+
+
 
     return (
         <>
             <Navbar expand='lg'>
                 <Container fluid>
                     <Navbar.Brand
-                    style={{ color: "#EFEA5A"}}>
+                        style={{ color: "#EFEA5A" }}>
                         Zero Stars!
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls='navbar' />
                     <Navbar.Collapse id='navbar'>
                         <Nav className='ml-auto'>
-                            <Nav.Link 
-                            style={{ color: "#EFEA5A"}}
-                            as={Link} to='/'>
+                            <Nav.Link
+                                style={{ color: "#EFEA5A" }}
+                                as={Link} to='/'>
                                 Search
                             </Nav.Link>
                             {/* if user is logged in show saved books and logout */}
                             {Auth.loggedIn() ? (
                                 <>
-                                    <Nav.Link 
-                                    style={{ color: "#EFEA5A"}}
-                                    as={Link} to='/profile'>
+                                    <Nav.Link
+                                        style={{ color: "#EFEA5A" }}
+                                        as={Link} to='/profile'>
                                         Profile
                                     </Nav.Link>
                                     <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                                 </>
                             ) : (
-                                <Nav.Link 
-                                style={{ color: "#EFEA5A"}}
-                                onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                                <Nav.Link
+                                    style={{ color: "#EFEA5A" }}
+                                    onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
                             )}
                         </Nav>
                     </Navbar.Collapse>
