2419a5460968bfa73a4bf391339fbf86f50fb26f
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-28 22:37:30 -0600

    began trying to render reviews into search component

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -68,21 +68,16 @@ function Search() {
             <div class="media">
               <div class="media-left">
                 <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                  <img src="https://i.ebayimg.com/thumbs/images/g/KXsAAOSwSJpgAbh2/s-l96.jpg" alt="Placeholder image of a hamburger" />
                 </figure>
               </div>
               <div class="media-content">
                 <p class="title is-4 is-black">{rest.name}</p>
-                <p class="subtitle is-6">@rest1</p>
               </div>
             </div>
 
             <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1" />11:09 PM - 1 Jan 2016
+              {rest.reviews.text}
             </div>
             {/* <div class="ui heart rating" data-rating="1" data-max-rating="3"></div> */}
           </div>
