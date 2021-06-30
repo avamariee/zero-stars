04b450eca2de1662632f67bddc1b527d2ac20e06
Tess Austin <tessaustin92@gmail.com>
2021-06-29 21:07:05 -0600

    added GET_ME query back, needed for profile

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,17 @@
 import gql from "graphql-tag";
 
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+        }
+    }
+`;
+
+
 export const QUERY_ME = gql`
     {
         me {
