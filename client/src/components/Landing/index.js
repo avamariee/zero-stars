4194feaafd99be926e4ded8321aedee322132f1f
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-06-30 20:19:29 -0600

    got search bar fixed

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -11,8 +11,8 @@ import { Link } from 'react-router-dom';
 
 function Landing() {
 
-  const [searchTerm, setSearchTerm] = useState(''); 
-  
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <div>
       <hero>
@@ -30,13 +30,23 @@ function Landing() {
           <div class="boxLanding my-5">
             <div class="field has-addons ">
               <div class="control searchBar is-centered">
-                <input class="input " type="text" placeholder="Find a Resturant" />
+                <input
+                  class="input "
+                  type="text"
+                  placeholder="Find a Resturant"
+                  onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm}
+                />
               </div>
               <div class="control">
-                <a class="button">
+              <a>
+                <Link
+                  to={`/search/${searchTerm}`}
+                  className="button"
+                  style={{ color: "#EFEA5A", background: "#4D9F8D" }} >
                   Search
-                </a>
-              </div>
+                </Link>
+              </a>
+            </div>
 
             </div>
           </div>
